621444bf101491096413924663bacec52b47f697
Unknown-Dolphin <162408564+Unknown-Dolphin@users.noreply.github.com>
2025-04-18 12:22:41 +0700

    more hint

--- Room/canvas_layer.gd
+++ Room/canvas_layer.gd
@@ -6,6 +6,6 @@ func _ready() -> void:
 	pass
 
 func control_gone() -> void:
-	await get_tree().create_timer(1).timeout
+	await get_tree().create_timer(6).timeout
 	var tween = create_tween()
 	tween.tween_property(control, "modulate:a", 0, 2.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
